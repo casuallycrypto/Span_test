2ae6542bc20634341c049e260b539a6d83c05a9c
JoshuaLeighton1 <joshuaroelf@gmail.com>
2024-11-18 05:42:02 +0200

    updated interface

--- ranking_table.py
+++ ranking_table.py
@@ -2,12 +2,13 @@ from utils import read_text, read_file
 
 
 if __name__ == "__main__":
-    print("Welcome to ranking table") 
+    print("Welcome to ranking table\nType 'm' to begin using text, type 'f' to begin using a file.") 
     inp = input("Upload via file or manuallyl?\n")
-    if inp == "manual":
+    if inp == "m":
+        print("enter the results here(Press Enter once you're done): \n")
         text = read_text()
-    elif inp == "file":
-        print("enter the file name here: ")
+    elif inp == "f":
+        print("enter the file name here: \n")
         file_input = read_file()
     else:
         print("I didn't quite get that, please choose a valid choice")
