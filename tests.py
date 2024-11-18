7f8197142309d16ba9b9838d84aa492644bb424e
JoshuaLeighton1 <joshuaroelf@gmail.com>
2024-11-18 06:01:26 +0200

    removed unused imports

--- tests.py
+++ tests.py
@@ -1,12 +1,13 @@
 import unittest
 
-from utils import display_ranking, decision, sorting, read_text, read_file
+from utils import sorting
 
 
 class TestUtilities(unittest.TestCase):
     def setUp(self):
         self.final = {}
         self.teams = ["Team_1 1", " Team_2 3"]
+        self.final_outcome = {'Foxes': 3, 'Tar': 1, 'Arks': 1, 'Hawks': 0}
         return super().setUp()
     def test_sorting(self):
         self.assertEqual(sorting(self.teams, self.final), {'Team_2': 3, 'Team_1': 0})
