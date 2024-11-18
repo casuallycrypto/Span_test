0e75a5513003a2101cf2f378012156b116da3474
JoshuaLeighton1 <joshuaroelf@gmail.com>
2024-11-18 06:16:45 +0200

    clean up var names

--- utils.py
+++ utils.py
@@ -43,51 +43,51 @@ def display_ranking(final_outcome):
         count+=1
 
 def sorting(teams, final_outcome):
-    scores = []
-    names = []
+    team_scores = []
+    team_names = []
     for team in teams:
         if team.split(" ")[0] == "":
             team = team.lstrip()
         else:
             team = team
         team_name = team.split(" ")[0]
-        names.append(team_name)
+        team_names.append(team_name)
         team_points = int(team.split(" ")[1])
         scores.append(team_points)
-    return decision(scores, names, final_outcome)
+    return decision(team_scores, team_names, final_outcome)
 
 #decision takes the outcomes and appends the score W=3, D=1, L=0
-def decision(scores,names, final_outcome):
-    if len(scores) == 1:
+def decision(team_scores,team_names, final_outcome):
+    if len(team_scores) == 1:
         return 
-    if scores[0] > scores[1]:
-        if names[0] in final_outcome.keys():
-            final_outcome[names[0]] = final_outcome.get(names[0]) + 3
+    if team_scores[0] > team_scores[1]:
+        if team_names[0] in final_outcome.keys():
+            final_outcome[team_names[0]] = final_outcome.get(team_names[0]) + 3
         else:
-            final_outcome[names[0]] = 3
-        if names[1] in final_outcome.keys():
-            final_outcome[names[1]] = final_outcome.get(names[1]) + 0
+            final_outcome[team_names[0]] = 3
+        if team_names[1] in final_outcome.keys():
+            final_outcome[team_names[1]] = final_outcome.get(team_names[1]) + 0
         else:
-            final_outcome[names[1]] = 0
-    if scores[0] < scores[1]:
-        if names[1] in final_outcome.keys():
-            final_outcome[names[1]] = final_outcome.get(names[1]) + 3
+            final_outcome[team_names[1]] = 0
+    if team_scores[0] < team_scores[1]:
+        if team_names[1] in final_outcome.keys():
+            final_outcome[team_names[1]] = final_outcome.get(team_names[1]) + 3
         else:
-            final_outcome[names[1]] = 3 
-        if names[0] in final_outcome.keys():
-            final_outcome[names[0]] = final_outcome.get(names[0]) + 0
+            final_outcome[team_names[1]] = 3 
+        if team_names[0] in final_outcome.keys():
+            final_outcome[team_names[0]] = final_outcome.get(team_names[0]) + 0
         else:
-            final_outcome[names[0]] = 0
-    if scores[0] == scores[1]:
-         if names[0] in final_outcome.keys():
-            final_outcome[names[0]] = final_outcome.get(names[0]) + 1
+            final_outcome[team_names[0]] = 0
+    if team_scores[0] == team_scores[1]:
+         if team_names[0] in final_outcome.keys():
+            final_outcome[team_names[0]] = final_outcome.get(team_names[0]) + 1
          else:
-             final_outcome[names[0]] = 1
+             final_outcome[team_names[0]] = 1
         
-         if names[1] in final_outcome.keys():
-             final_outcome[names[1]] = final_outcome.get(names[1]) + 1
+         if team_names[1] in final_outcome.keys():
+             final_outcome[team_names[1]] = final_outcome.get(team_names[1]) + 1
          else:
-             final_outcome[names[1]] = 1
+             final_outcome[team_names[1]] = 1
        
     return final_outcome
         
