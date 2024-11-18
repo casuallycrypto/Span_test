26a70654fa69d51c4777e1e753317e726e317dd5
JoshuaLeighton1 <joshuaroelf@gmail.com>
2024-11-18 05:45:51 +0200

    test updated

--- tests.py
+++ tests.py
@@ -4,12 +4,12 @@ from utils import display_ranking, decision, sorting, read_text, read_file
 
 
 class TestUtilities(unittest.TestCase):
-    def setUp(self) -> None:
+    def setUp(self):
         self.final = {}
-        self.teams = ["Team_1 1, Team_2 3"]
+        self.teams = ["Team_1 1", " Team_2 3"]
         return super().setUp()
     def test_sorting(self):
-        self.assertEqual(sorting(self.teams), "1. Team_2 3\n2.Team_1 0\n")
+        self.assertEqual(sorting(self.teams, self.final), {'Team_2': 3, 'Team_1': 0})
 
 
 
